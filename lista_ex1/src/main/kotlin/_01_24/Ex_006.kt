72bb2c879623189c3a66845940e97f3fb636b94b
francisco paes ramos junior <francisco.junior@ipdec.org>
2020-10-27 16:59:56 -0400

    resolvido questões 6 e 7

--- lista_ex1/src/main/kotlin/_01_24/Ex_006.kt
+++ lista_ex1/src/main/kotlin/_01_24/Ex_006.kt
@@ -1,2 +1,25 @@
 package _01_24
 
+/*Construa uma função que verifique se um dado número é divisível por outro. Ambos devem ser
+fornecidos pelo usuário.*/
+
+fun main() {
+    print("numero 1: ")
+    var numero1 = readLine()!!.toString().toInt()
+
+    print("numero 2: ")
+    var numero2 = readLine()!!.toString().toInt()
+
+    print("Pode dividir: " + podeDividir(numero1, numero2))
+}
+
+fun podeDividir(num1:Int, num2:Int):String{
+    if(num1 % num2 != 0){
+        return ("não")
+    }
+    return ("sim")
+}
+
+
+
+
--- lista_ex1/src/main/kotlin/_01_24/Ex_007.kt
+++ lista_ex1/src/main/kotlin/_01_24/Ex_007.kt
@@ -1,2 +1,17 @@
 package _01_24
+/*Faça uma função que possibilite o arredondamento de um número real para um número inteiro
+seguindo os padrões científicos.*/
 
+
+fun main() {
+
+    print("Numero: ")
+    var numero = readLine()!!.toString().toDouble()
+
+    print("Arredondado: " + arredondar(numero))
+}
+
+fun arredondar(numero: Double): Double {
+
+    return Math.round(numero).toDouble()
+}
