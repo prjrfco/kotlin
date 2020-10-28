3845fbbc7e3189db082db26c8a80c23faf00b72a
francisco paes ramos junior <francisco.junior@ipdec.org>
2020-10-28 09:33:01 -0400

    resolvido questão 8 e 9

--- lista_ex1/src/main/kotlin/_01_24/Ex_008.kt
+++ lista_ex1/src/main/kotlin/_01_24/Ex_008.kt
@@ -1,2 +1,31 @@
 package _01_24
 
+/*Elabore uma função que escreva por extenso um número inteiro com até dez dígitos fornecido pelo
+usuário.*/
+
+fun main() {
+
+    print("Digite um número: ")
+
+    var numeroDig: Int = readLine()!!.toString().toInt()
+
+    print("Por extenso: " + porExtenso(numeroDig))
+
+}
+
+fun porExtenso(numeroDig: Int): String {
+    var result = ""
+    val numeros = arrayOf("zero", "um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove")
+
+    val numeroString = numeroDig.toString()
+
+    for (i in 0 .. numeroString.length-1){
+        var n = Character.getNumericValue(numeroString[i])
+        result += numeros[n]+" "
+    }
+    return result
+}
+
+
+
+
--- lista_ex1/src/main/kotlin/_01_24/Ex_009.kt
+++ lista_ex1/src/main/kotlin/_01_24/Ex_009.kt
@@ -1,2 +1,20 @@
 package _01_24
 
+/*Construa uma função que verifique, sem utilizar a função mod, se um número é divisível por outro.*/
+
+fun main() {
+    print(podeDividirSemMod(7,2))
+    print(podeDividirSemMod(6,2))
+
+}
+
+
+fun podeDividirSemMod(num1:Int, num2:Int):String{
+    var resultadoInt:Int
+    resultadoInt = num1 / num2
+    print("ResultadoInt: $resultadoInt")
+    if(resultadoInt * num2 == num1){
+        return "sim"
+    }
+    return "não"
+}
