9b2bebca9b043f14844a830210368adee0ee73b6
francisco paes ramos junior <francisco.junior@ipdec.org>
2020-10-29 11:53:14 -0400

    resolvido questão 16

--- lista_ex1/src/main/kotlin/_01_24/Ex_016.kt
+++ lista_ex1/src/main/kotlin/_01_24/Ex_016.kt
@@ -1,2 +1,18 @@
 package _01_24
 
+/*Construa uma função que identifique se um número é ou não primo.*/
+
+fun main() {
+
+    print("Digite um número: ")
+    val numero:Int = readLine()!!.toString().toInt()
+
+    print("\n\nÉ primo: "+ ehPrimo(numero))
+}
+
+fun ehPrimo(numero: Int): Boolean {
+    for (i in 2 until numero) {
+        if (numero % i == 0) return false
+    }
+    return true
+}
