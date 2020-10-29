eac4b3a571ecae513001b95903313f2ae04b8bbe
francisco paes ramos junior <francisco.junior@ipdec.org>
2020-10-29 08:54:37 -0400

    resolvido questão 13

--- lista_ex1/src/main/kotlin/_01_24/Ex_013.kt
+++ lista_ex1/src/main/kotlin/_01_24/Ex_013.kt
@@ -1,7 +1,5 @@
 package _01_24
 
-import java.util.*
-
 /*Faça a leitura de mil números pelo processo de sorteio automático. Os números devem estar entre 0 e
 100. Verifique:
 
@@ -12,19 +10,37 @@ import java.util.*
 
 fun main() {
     var freqs = Array(101, { i -> 0 })
+    println("Lista Aleatória: \n")
     val lista = aleatorio(1000, 0, 101)
-    var maior = -1;
-    var menor = 101;
-    println(lista)
-    lista.forEach{numero ->
-        if(numero > maior) maior = numero
-        if(numero < menor) menor = numero
+    var maior = -1
+    var menor = 101
+
+    lista.forEach { numero ->
+        if (numero > maior) maior = numero
+        if (numero < menor) menor = numero
         freqs[numero] += 1
-
     }
-    println("\nFreqs: " + Arrays.toString(freqs))
-    println("Maior: $maior")
+
+    var maiorFrequencia = 0
+    var maisFrequente = 0
+
+    var menorFrequencia = 1000000000
+    var menosFrequente = 0
+
+    for (i in 0..100) {
+        print("(n=$i, f=" + freqs[i] + ") ")
+        if (freqs[i] > maiorFrequencia) {
+            maiorFrequencia = freqs[i]
+            maisFrequente = i;
+        }
+
+        if (freqs[i] > 0 && freqs[i] < menorFrequencia) {
+            menorFrequencia = freqs[i]
+            menosFrequente = i;
+        }
+    }
+    println("\n\nMaior: $maior")
     println("Menor: $menor")
-
-
+    println("\nNúmero mais frequente: $maisFrequente (f=$maiorFrequencia)")
+    println("Número menos frequente: $menosFrequente (f=$menorFrequencia)")
 }
