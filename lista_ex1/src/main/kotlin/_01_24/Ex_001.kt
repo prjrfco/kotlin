52f09cb16f3c5d68dee8c8f22f2bb3ad6b73fa58
Francisco <prjr.francisco@gmail.com>
2020-11-02 13:04:53 -0400

    fix da questão 1

--- lista_ex1/src/main/kotlin/_01_24/Ex_001.kt
+++ lista_ex1/src/main/kotlin/_01_24/Ex_001.kt
@@ -1,4 +1,7 @@
 package _01_24
+
+import kotlin.math.pow
+
 /* Crie um aplicativo que receba o raio de uma esfera (do tipo double) e chame o método volumeEsfera
 para calcular e exibir o volume da esfera na tela. Para cálculo do volume deve ser usada a fórmula:
 volume = (4.0/3.0)*pi*raio 2 .*/
@@ -15,7 +18,7 @@ fun main() {
 fun volumeEsfera(raio: Double? = null): Double? {
     if(raio != null){
         val pi: Double = 3.14159
-        return (4.0 / 3.0) * pi * raio
+        return (4.0 / 3.0) * pi * raio.pow(2)
     }
     return null
 }
